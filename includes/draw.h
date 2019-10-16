e91dae573bd8053cbc09f34bb3cb32a8043e95b1
qsharoly <qsharoly@student.21-school.ru>
2019-10-16 18:12:57 +0300

    add t_view (2 pointers: bitmap and cam) to make draw_edge fit 4 params per func Norme

--- includes/draw.h
+++ includes/draw.h
@@ -18,11 +18,17 @@
 # include "vertex.h"
 # include "projection.h"
 
+typedef struct	s_view
+{
+	t_bitmap	*bmp;
+	t_cam		*cam;
+}		t_view;
+
 t_rgba	mix(t_rgba col1, t_rgba col2, float ratio);
 int		inbounds(t_float2 point, t_bitmap *bmp);
 void	do_blend(t_bitmap *bmp, t_float2 p, t_rgba color);
 void	draw_line(t_bitmap *bmp, t_float2 a, t_float2 b, t_rgba color);
-void	draw_edge(t_bitmap *bmp, t_cam *cam, t_float3 a, t_float3 b, t_rgba color);
+void	draw_edge(t_view view, t_float3 a, t_float3 b, t_rgba color);
 void	draw_edge_gradient(t_bitmap *bmp, t_cam *cam, t_vertex a, t_vertex b);
 void	draw_edge_gradient_z_buf(t_bitmap *bmp, t_cam *cam, t_vertex a, t_vertex b);
 
