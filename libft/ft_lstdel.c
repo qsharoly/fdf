7af6803033595bd9996d53e114edac53105c2f58
Quaynis Sharolyn <qsharoly@student.21-school.ru>
2019-10-20 19:32:46 +0300

    add NULL checks

--- libft/ft_lstdel.c
+++ libft/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: qsharoly <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 13:51:19 by qsharoly          #+#    #+#             */
-/*   Updated: 2019/09/11 16:44:19 by qsharoly         ###   ########.fr       */
+/*   Updated: 2019/10/20 19:30:13 by qsharoly         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
+	if (alst == NULL || *alst == NULL)
+		return ;
 	if ((*alst)->next == NULL)
 		ft_lstdelone(alst, del);
 	else
